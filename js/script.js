f544aa0920375981d1550669fc4572930a98c9c0
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-05-26 19:51:13 -0300

    26/05/24

--- js/script.js
+++ js/script.js
@@ -11,5 +11,47 @@ function initMenuMobile() {
   }
 }
 initMenuMobile()
-
 /*MENU MOBILE */
+/*ACCORDION */
+const accordion = document.querySelectorAll('.accordion')
+function initAccordion() {
+  if (accordion) {
+    accordion.forEach(item => {
+      const question = item.querySelector('.question')
+      const answer = item.querySelector('.answer')
+
+      answer.style.maxHeight = 0
+
+      question.addEventListener('click', () => {
+        if (item.classList.contains('open')) {
+          answer.style.maxHeight = 0
+          item.classList.remove('open')
+        } else {
+          answer.style.maxHeight =
+            answer.querySelector('.content').scrollHeight + 'px'
+          item.classList.add('open')
+        }
+      })
+    })
+  }
+}
+initAccordion()
+/*END ACCORDION */
+/*SWIPER */
+const swiperApp = new Swiper('.swiper-app .swiper', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-app .button-next',
+    prevEl: '.swiper-app .button-prev'
+  }
+})
+const swiperComments = new Swiper('.swiper-comments .swiper', {
+  loop: true,
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.swiper-comments .button-next',
+    prevEl: '.swiper-comments .button-prev'
+  }
+})
+/*SWIPER */
